eed764ffee0b5c86dd8d1da9f7e75f5f8c1f0c11
HuongNguyenQuoc <quochuong05ln@gmail.com>
2026-08-11 20:17:56 +0700

    initialize fanout-worker and tweet-write-service modules with basic configurations and dependencies

---
+++ tweet-write-service/src/main/java/com/example/tweet_write_service/TweetWriteServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.tweet_write_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TweetWriteServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TweetWriteServiceApplication.class, args);
+	}
+
+}
---
+++ tweet-write-service/src/test/java/com/example/tweet_write_service/TweetWriteServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tweet_write_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TweetWriteServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
