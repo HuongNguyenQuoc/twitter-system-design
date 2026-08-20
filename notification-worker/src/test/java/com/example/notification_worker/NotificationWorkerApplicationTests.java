14bf3b6ac1a062a6e2665703c947f7c131d378b8
HuongNguyenQuoc <quochuong05ln@gmail.com>
2026-08-20 21:41:58 +0700

    add: initialize project structure with Maven wrapper and basic application setup

---
+++ notification-worker/src/test/java/com/example/notification_worker/NotificationWorkerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.notification_worker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationWorkerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
